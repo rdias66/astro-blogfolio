e07f98fa6dd4114c0621f1a04d08d1f0db29bb1f
rdias66 <dias.a@live.com>
2024-09-27 19:51:26 -0300

    navigation ui done

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -4,7 +4,7 @@ document.querySelector('.search-button').addEventListener('click', () => {
 
 document.querySelector('.search-overlay').addEventListener('click', (event) => {
   const searchBox = document.querySelector('.search-box')
-  // Check if the click was outside the search box
+
   if (!searchBox.contains(event.target)) {
     document.querySelector('.search-overlay').classList.remove('active')
   }
