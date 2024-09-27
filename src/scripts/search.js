4f07abfad3d5f5afa022289a043cf93d977f7aad
rdias66 <dias.a@live.com>
2024-09-27 18:59:46 -0300

    ux improved for searchbox's closing

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -2,6 +2,10 @@ document.querySelector('.search-button').addEventListener('click', () => {
   document.querySelector('.search-overlay').classList.toggle('active')
 })
 
-document.querySelector('.close-button').addEventListener('click', () => {
-  document.querySelector('.search-overlay').classList.toggle('active')
+document.querySelector('.search-overlay').addEventListener('click', (event) => {
+  const searchBox = document.querySelector('.search-box')
+  // Check if the click was outside the search box
+  if (!searchBox.contains(event.target)) {
+    document.querySelector('.search-overlay').classList.remove('active')
+  }
 })
