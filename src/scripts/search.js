95474d51b9f5a0484ad585fe7ca60c35c25ed6e6
rdias66 <dias.a@live.com>
2024-09-27 18:55:37 -0300

    search bar flow done + first styles

---
+++ src/scripts/search.js
@@ -0,0 +1,7 @@
+document.querySelector('.search-button').addEventListener('click', () => {
+  document.querySelector('.search-overlay').classList.toggle('active')
+})
+
+document.querySelector('.close-button').addEventListener('click', () => {
+  document.querySelector('.search-overlay').classList.toggle('active')
+})
